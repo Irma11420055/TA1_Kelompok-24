cf778e006fdd44600e46b40f99f75fbc3e6d587e
Irma11420055 <82867241+Irma11420055@users.noreply.github.com>
2024-05-06 08:19:12 +0700

    managemen artikel

--- resources/views/backend/articles/index.blade.php
+++ resources/views/backend/articles/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.backend.master')
-@section('title', 'Buku')
+@section('title', 'Artikel')
 @section('breadcrumb')
     <ol class="breadcrumb float-sm-right">
         <li class="breadcrumb-item"><a href="#">Home</a></li>
-        <li class="breadcrumb-item active">Buku</li>
+        <li class="breadcrumb-item active">Artikel</li>
     </ol>
 @endsection
 @section('content')
@@ -14,29 +14,24 @@
                 <x-alert />
                 <div class="card">
                     <div class="card-header">
-                        {{-- <h3 class="card-title">Total Buku : {{ $totalBooks }} Judul, {{ $totalCopies }} Eksemplar</h3> --}}
-                        <h1 class="card-title"><span class="badge badge-primary">Total Buku: {{ $totalBooks }} Judul</span>
-                            <span class="badge badge-info">Total Eksemplar: {{ $totalCopies }}</span>
-                        </h1>
                         <div class="card-tools">
-                            <a href="{{ route('backend.books.create') }}" class="btn btn-primary">Tambah Buku</a>
+                            <a href="{{ route('backend.articles.create') }}" class="btn btn-primary">Tambah Artikel</a>
                         </div>
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
-                        <table id="book_datatable" class="table table-head-fixed">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Judul Buku</th>
-                                    <th>Bahasa</th>
-                                    <th>Pengarang</th>
-                                    <th>Penerbit</th>
-                                    <th>Tahun</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                        </table>
+                        <div class="table-responsive">
+                            <table id="article_datatable" class="table table-head-fixed">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Judul</th>
+                                        <th>Isi</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
                     </div>
                     <!-- /.card-body -->
                 </div>
@@ -50,7 +45,7 @@
     <script>
         $(document).ready(function() {
             const url = window.location.href;
-            const table = $('#book_datatable').DataTable({
+            const table = $('#article_datatable').DataTable({
                 processing: true,
                 serverSide: true,
                 ajax: url,
@@ -65,40 +60,38 @@
                         name: 'title'
                     },
                     {
-                        data: 'language',
-                        name: 'language'
+                        data: 'body',
+                        name: 'body'
                     },
                     {
-                        data: 'author',
-                        name: 'author'
-                    },
-                    {
-                        data: 'publisher',
-                        name: 'publisher'
-                    },
-                    {
-                        data: 'year',
-                        name: 'year'
-                    }, {
-                        data: 'action',
                         name: 'action',
                         orderable: false,
                         searchable: false
                     }
                 ],
                 columnDefs: [{
-                    targets: 0,
-                    className: 'text-center',
-                    width: '5%',
-                    render: function(data, type, row, meta) {
-                        return meta.row + meta.settings._iDisplayStart + 1;
-                    }
-                }, {
-                    targets: 6,
-                    className: 'text-center',
-                    width: '15%',
-                    render: function(data, type, row) {
-                        return `
+                        targets: 0,
+                        className: 'text-center',
+                        width: '5%',
+                        render: function(data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        }
+                    },
+                    {
+                        targets: 2,
+                        className: 'text-center',
+                        orderable: false,
+                        render: function(data, type, row) {
+                            // render html
+                            return `<div class="text-center">${data.substring(0, 100)}...</div>`;
+                        }
+                    },
+                    {
+                        targets: 3,
+                        className: 'text-center',
+                        orderable: false,
+                        render: function(data, type, row) {
+                            return `
                             <div class="btn-group">
                                 <a class="btn btn-info btn-sm" href="${url}/${row.id}">
                                     <i class="fas fa-eye"></i>
@@ -111,22 +104,24 @@
                                 </a>
                             </div>
                         `;
-                    },
-                }],
+                        },
+                        width: '15%'
+                    }
+                ],
                 searching: true,
                 order: [
                     [0, 'desc']
                 ]
             });
 
-            $('#book_datatable').on('click', '.btn-delete', function(e) {
+            $('#article_datatable').on('click', '.btn-delete', function(e) {
                 e.preventDefault();
                 const url = $(this).attr('href');
                 showConfirmationDialog('Are you sure?', 'You won\'t be able to revert this!', 'warning',
                     'Yes, delete it!', (result) => {
                         if (result.isConfirmed) {
-                            handleAction(url, 'DELETE', 'Book has been deleted!',
-                                'Failed to delete category!', {}, null, () => {
+                            handleAction(url, 'DELETE', 'Article has been deleted!',
+                                'Failed to delete article!', {}, null, () => {
                                     table.ajax.reload();
                                 });
                         }
